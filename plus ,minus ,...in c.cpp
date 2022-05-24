b71b16246284cab5c3049734cd6ee9fe9a60fb6d
ahadi818 <106049641+ahadi818@users.noreply.github.com>
2022-05-24 03:37:03 -0700

    plus , minus ....., in c

---
+++ plus ,minus ,...in c.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,b,plus,minus,multy,diverse;
+    printf("enter the number ");
+    scanf("%d",&a);
+    printf("enter the number ");
+    scanf("%d",&b);
+    plus=a+b;
+    minus=a-b;
+    multy=a*b;
+    diverse=a/b;
+    printf("%d %d %d %d",plus,minus,multy,diverse);
+    return 0;
+    
+    
+}
